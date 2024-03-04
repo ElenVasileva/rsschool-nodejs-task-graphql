44d7645f3a876c56d4c2ead0f21b5b273ab486fa
Elena <elen.vasileva@gmail.com>
2024-03-04 23:27:32 +0100

    test-mutations (without the last)

--- src/routes/graphql/index.ts
+++ src/routes/graphql/index.ts
@@ -1,11 +1,8 @@
 import { FastifyPluginAsyncTypebox } from '@fastify/type-provider-typebox';
 import { createGqlResponseSchema, gqlResponseSchema } from './schemas.js';
-import { GraphQLList, GraphQLObjectType, GraphQLSchema, graphql } from 'graphql';
-import { UUIDType } from './types/uuid.js';
-import { MemberType, MemberTypeId } from './types/memberType.js';
-import { Post } from './types/post.js';
-import { Profile } from './types/profile.js';
-import { User } from './types/user.js';
+import { GraphQLSchema, graphql } from 'graphql';
+import { mutationSchema } from './mutationSchema.js';
+import { querySchema } from './querySchema.js';
 
 
 const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
@@ -22,85 +19,8 @@ const plugin: FastifyPluginAsyncTypebox = async (fastify) => {
 
       return await graphql({
         schema: new GraphQLSchema({
-          query: new GraphQLObjectType({
-            name: 'Query',
-            fields: () => ({
-              memberTypes: {
-                type: new GraphQLList(MemberType),
-                resolve: async () => { return await fastify.prisma.memberType.findMany() },
-              },
-              memberType: {
-                type: MemberType,
-                args: {
-                  id: {
-                    type: MemberTypeId,
-                  },
-                },
-                resolve: async (_, { id }: { id: string }) => {
-                  try {
-                    return await fastify.prisma.memberType.findUnique({ where: { id } })
-                  }
-                  catch (error) { return new Error('error') }
-                },
-              },
-              users: {
-                type: new GraphQLList(User),
-                resolve: async () => { return await fastify.prisma.user.findMany() },
-              },
-              user: {
-                type: User,
-                args: {
-                  id: {
-                    type: UUIDType,
-                  },
-                },
-                resolve: async (_, { id }: { id: string }) => {
-                  try {
-                    const user = await fastify.prisma.user.findUnique({ where: { id } })
-                    return user
-                  }
-                  catch (error) { return new Error('error') }
-                },
-              },
-              profiles: {
-                type: new GraphQLList(Profile),
-                resolve: async () => { return await fastify.prisma.profile.findMany() },
-              },
-              profile: {
-                type: Profile,
-                args: {
-                  id: {
-                    type: UUIDType,
-                  },
-                },
-                resolve: async (_, { id }: { id: string }) => {
-                  try {
-                    return await fastify.prisma.profile.findUnique({ where: { id } })
-                  }
-                  catch (error) { return new Error('error') }
-
-                },
-              },
-              posts: {
-                type: new GraphQLList(Post),
-                resolve: async () => { return await fastify.prisma.post.findMany() },
-              },
-              post: {
-                type: Post,
-                args: {
-                  id: {
-                    type: UUIDType,
-                  },
-                },
-                resolve: async (_, { id }: { id: string }) => {
-                  try {
-                    return await fastify.prisma.post.findUnique({ where: { id } })
-                  }
-                  catch (error) { return new Error('error') }
-                },
-              },
-            }),
-          })
+          query: querySchema,
+          mutation: mutationSchema
         }),
         source: req.body.query,
         variableValues: req.body.variables,
---
+++ src/routes/graphql/mutationSchema.ts
@@ -0,0 +1,238 @@
+import { PrismaClient } from "@prisma/client"
+import { GraphQLInputObjectType, GraphQLString, GraphQLFloat, GraphQLBoolean, GraphQLInt, GraphQLObjectType } from "graphql"
+import { HttpCompatibleError } from "../../plugins/handle-http-error.js"
+import { Post } from "./types/post.js"
+import { Profile } from "./types/profile.js"
+import { User } from "./types/user.js"
+import { UUIDType } from "./types/uuid.js"
+
+
+const CreateUserInput = new GraphQLInputObjectType({
+    name: "CreateUserInput",
+    fields: {
+        name: {
+            type: GraphQLString,
+        },
+        balance: {
+            type: GraphQLFloat
+        }
+    }
+})
+
+const ChangeUserInput = new GraphQLInputObjectType({
+    name: "ChangeUserInput",
+    fields: {
+        name: {
+            type: GraphQLString,
+        },
+        balance: {
+            type: GraphQLFloat
+        }
+    }
+})
+
+const CreateProfileInput = new GraphQLInputObjectType({
+    name: "CreateProfileInput",
+    fields: {
+        isMale: {
+            type: GraphQLBoolean,
+        },
+        yearOfBirth: {
+            type: GraphQLInt
+        },
+        userId: {
+            type: GraphQLString
+        },
+        memberTypeId: {
+            type: GraphQLString
+        },
+    }
+})
+
+const ChangeProfileInput = new GraphQLInputObjectType({
+    name: "ChangeProfileInput",
+    fields: {
+        isMale: {
+            type: GraphQLBoolean,
+        },
+        yearOfBirth: {
+            type: GraphQLInt
+        },
+        userId: {
+            type: GraphQLString
+        },
+        memberTypeId: {
+            type: GraphQLString
+        },
+    }
+})
+
+const CreatePostInput = new GraphQLInputObjectType({
+    name: "CreatePostInput",
+    fields: {
+        title: {
+            type: GraphQLString
+        },
+        content: {
+            type: GraphQLString
+        },
+        authorId: {
+            type: GraphQLString
+        },
+    }
+})
+
+const ChangePostInput = new GraphQLInputObjectType({
+    name: "ChangePostInput",
+    fields: {
+        title: {
+            type: GraphQLString
+        },
+        content: {
+            type: GraphQLString
+        },
+        authorId: {
+            type: GraphQLString
+        },
+    }
+})
+
+export const mutationSchema = new GraphQLObjectType({
+    name: "Mutation",
+    fields: {
+
+        createUser: {
+            type: User,
+            args: {
+                dto: { type: CreateUserInput }
+            },
+            resolve: async (_, { dto }: { dto: { name: string, balance: number } }, { prisma }: { prisma: PrismaClient }) => {
+                try {
+                    return await prisma.user.create({ data: dto })
+                } catch (err) {
+                    throw new Error('error');
+                }
+            },
+        },
+
+        changeUser: {
+            type: User,
+            args: {
+                id: { type: UUIDType },
+                dto: { type: ChangeUserInput }
+            },
+            resolve: async (_, { id, dto }: { id: string, dto: { name: string, balance: number } }, { prisma }: { prisma: PrismaClient }) => {
+                try {
+                    return await prisma.user.update({
+                        where: { id },
+                        data: dto,
+                    })
+                } catch (err) {
+                    throw new Error('error');
+                }
+            },
+        },
+
+        deleteUser: {
+            type: GraphQLBoolean,
+            args: {
+                id: { type: UUIDType },
+            },
+            resolve: async (_, { id }: { id: string }, { prisma }: { prisma: PrismaClient }) => {
+                await prisma.user.delete({ where: { id } })
+            },
+        },
+
+        createProfile: {
+            type: Profile,
+            args: {
+                dto: { type: CreateProfileInput }
+            },
+            resolve: async (_, { dto }: { dto: { isMale: boolean, yearOfBirth: number, userId: string, memberTypeId: string } }, { prisma }: { prisma: PrismaClient }) => {
+                try {
+                    const a = await prisma.profile.create({ data: dto })
+                    return a
+                } catch (err) {
+                    throw new Error('error');
+                }
+            },
+        },
+
+        changeProfile: {
+            type: Profile,
+            args: {
+                id: { type: UUIDType },
+                dto: { type: ChangeProfileInput }
+            },
+            resolve: async (_, { id, dto }: { id: string, dto: { isMale: boolean, yearOfBirth: number, userId: string, memberTypeId: string } }, { prisma }: { prisma: PrismaClient }) => {
+                try {
+                    console.log('changeProfile: -------------------------------------------')
+                    return await prisma.profile.update({
+                        where: { id },
+                        data: dto,
+                    })
+                } catch (err) {
+                    const error = err as HttpCompatibleError
+                    if (error.httpCode === 422)
+                        throw new Error(`Field "userId" is not defined by type "ChangeProfileInput"`)
+                    throw new Error(error.message)
+                }
+            },
+        },
+
+        deleteProfile: {
+            type: GraphQLBoolean,
+            args: {
+                id: { type: UUIDType, },
+            },
+            resolve: async (_, { id }: { id: string }, { prisma }: { prisma: PrismaClient }) => {
+                await prisma.profile.delete({ where: { id } })
+            },
+        },
+
+        createPost: {
+            type: Post,
+            args: {
+                dto: { type: CreatePostInput }
+            },
+            resolve: async (_, { dto }: { dto: { title: string, content: string, authorId: string } }, { prisma }: { prisma: PrismaClient }) => {
+                try {
+                    return await prisma.post.create({ data: dto })
+                } catch (err) {
+                    throw new Error('error');
+                }
+            },
+        },
+
+        changePost: {
+            type: Post,
+            args: {
+                id: { type: UUIDType },
+                dto: { type: ChangePostInput }
+            },
+            resolve: async (_, { id, dto }: { id: string, dto: { title: string, content: string, authorId: string } }, { prisma }: { prisma: PrismaClient }) => {
+                console.log('changePost: -------------------------------------------')
+                try {
+                    return await prisma.post.update({
+                        where: { id },
+                        data: dto,
+                    })
+                } catch (err) {
+                    throw new Error('error');
+                }
+            },
+        },
+
+        deletePost: {
+            type: GraphQLBoolean,
+            args: {
+                id: {
+                    type: UUIDType,
+                },
+            },
+            resolve: async (_, { id }: { id: string }, { prisma }: { prisma: PrismaClient }) => {
+                await prisma.post.delete({ where: { id } })
+            },
+        },
+    }
+})
